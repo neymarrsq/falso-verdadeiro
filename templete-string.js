68eaf24afdce2ef8e2303455e4a4179d2508f6a4
Neyma <neymar.queiroz@escola.pr.gov.br>
2026-05-04 15:23:22 +0000

    .

---
+++ templete-string.js
@@ -0,0 +1,6 @@
+const nome = "Doutor Neymar";
+const idade = 2026 - 2012;
+const cidadeDeNascimento = "Joinville";
+
+const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+console.log(apresentacao)
